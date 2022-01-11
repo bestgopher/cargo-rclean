75660d4d97eddf41b76e35179c6e4323a58b4594
bestgopher <84328409@qq.com>
2022-01-11 16:06:57 +0800

    v0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod walk;
+
+use cargo::ops::{clean, CleanOptions};
+use cargo::core::Workspace;
+use cargo::util::{Config, interning::InternedString};
+
+
+use crate::walk::Walk;
+
+fn main() -> anyhow::Result<()> {
+    for path in Walk::new(".") {
+        let config = &Config::default()?;
+        let w = Workspace::new(path.join("Cargo.toml").as_path(), config)?;
+        let config_opt: CleanOptions = CleanOptions {
+            config: &config,
+            spec: Default::default(),
+            targets: Default::default(),
+            profile_specified: Default::default(),
+            requested_profile: InternedString::from("dev"),
+            doc: Default::default(),
+        };
+
+        clean(&w, &config_opt)?;
+    }
+
+    Ok(())
 }
---
+++ src/walk.rs
@@ -0,0 +1,58 @@
+use std::collections::LinkedList;
+use std::path::{Path, PathBuf};
+use std::fs;
+
+pub struct Walk {
+    list: LinkedList<PathBuf>,
+}
+
+impl Walk {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let mut s = Walk { list: LinkedList::new() };
+        if path.as_ref().to_path_buf().is_absolute() {
+            s.list.push_front(path.as_ref().to_path_buf());
+        } else {
+            s.list.push_front(fs::canonicalize(path.as_ref().to_path_buf()).unwrap().to_path_buf());
+        }
+        s
+    }
+}
+
+impl Iterator for Walk {
+    type Item = PathBuf;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.list.pop_front() {
+                Some(path) => {
+                    if path.is_dir() {
+                        if path.join("Cargo.toml").exists() {
+                            return Some(path);
+                        } else {
+                            if let Ok(f) = path.read_dir() {
+                                self.list.extend(
+                                    f
+                                        .filter(|x| x.is_ok() && x.as_ref().unwrap().path().is_dir())
+                                        .map(|x| x.unwrap().path()))
+                            }
+                        }
+                    }
+                }
+                None => break,
+            }
+        }
+
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::walk::Walk;
+
+    #[test]
+    fn test_walk() {
+        for i in Walk::new("../.") {
+            println!("{:?}", i.as_path().display());
+        }
+    }
+}
